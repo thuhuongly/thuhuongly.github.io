dcb7717810138b03a6f38a41aa6dfe76605fcacb
Ly Thu Huong <lythuhuong1190@gmail.com>
2021-09-25 02:39:12 +0200

    Add _site
    With the hope that the site is not blank

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Vì sao tôi ở đây",
+        "excerpt":"Tối nay tôi không muốn hay thực chất là tôi sợ mở điện thoại lên, bật wifi và hàng tá tin nhắn; hoặc vô tình đăng nhập vào Facebook rồi tốn hàng giờ ở đó. Lí do là vì tôi không muốn hủy hoại sự bình yên của tâm trí,...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2016/12/22/why-i-stay.html",
+        "teaser": null
+      },{
+        "title": "Mơ thi",
+        "excerpt":"Đêm qua ngủ mơ mình đi thi Văn, viết mãi mới được có 3 mặt giấy mà hỏi đứa bên cạnh nó bảo nó viết được 10 trang rồi. Mình hỏi nó 10 trang là 10 mặt hay 20 mặt, nó bảo 20 😭. Sợ toát mồ hôi, giật mình...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2017/06/22/thi-van.html",
+        "teaser": null
+      },{
+        "title": "Bầu trời",
+        "excerpt":"Có một việc mà tôi làm không bao giờ chán đó là nhìn ngắm bầu trời, đặc biệt là bầu trời chiều. Như lúc này, mỗi khi có cơ hội nhìn ngắm bầu trời sau khi tắt nắng, trong đầu tôi lại hiện lên cái tựa sách “Bầu trời chiều...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2017/07/09/bautroi.html",
+        "teaser": null
+      },{
+        "title": "Khổ",
+        "excerpt":"Tôi có thằng đồng nghiệp suốt ngày bày đồ ăn trên bàn làm việc. Mỗi lần nó chuẩn bị ăn cái gì là tôi lại hỏi: “Mày đang khổ cái gì à? Code của mày không chạy à? Mày ăn để cho vơi sầu đau đúng không?” Có lúc nó...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2018/04/28/kho.html",
+        "teaser": null
+      },{
+        "title": "Cánh chim trời",
+        "excerpt":"“Những cánh hồng sân ngoài một chiều mây bay gió lay tóc thề người đang còn thơ bé Như chim trời người trẻ dại trong ta líu lo bên thềm những khúc ca mùa xuân…” Cho đến giờ, trong tôi vẫn còn hình ảnh những buổi chiều muộn ngồi ở...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2018/05/14/canh-chim-troi.html",
+        "teaser": null
+      }]
